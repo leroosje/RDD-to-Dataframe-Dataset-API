07dbbe85d924bc4865d4c681abcd6ec8bed031cf
leroosje <leroosje@uncg.edu>
2018-11-08 13:50:23 -0500

    Restructured tokenization around the Scanner code provided in HW1

---
+++ src/TokenNames.java
@@ -0,0 +1,13 @@
+/**
+ * A enum of the token names
+ */
+
+/**
+ * @author Danny Reinheimer
+ * Edited by Luke Roosje within the context of this project.
+ */
+public enum TokenNames {
+	Number, Identifier, String, Character, Symbol,
+	MetaStatements, None
+
+}
