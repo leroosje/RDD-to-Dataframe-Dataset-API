84d335bc95cb023a666b373c9da0377d857c58ea
Qiaodan Wang <qwang32@ncsu.edu>
2018-12-07 23:01:57 -0500

    add testcases

--- part2/README.md
+++ part2/README.md
@@ -1,1 +1,2 @@
 # RDD-to-Dataframe
+To run the tests in Main.java, please use Java 1.8 for both source code and target.
--- part2/src/main/java/Main.java
+++ part2/src/main/java/Main.java
@@ -18,16 +18,19 @@ public class Main {
     }
 
     public static void main(String[] args){
-        String test = "sc.range(10,100).map(x=>{val z=x%2;(x+2,if(z+3>0) x else y)}).collect()";
-        String test1 = "sc.range(10,100)\n" +
-                "  .map(r=>r._1+r._2)\n" +
-                ".collect()\n";
+        String test1 = "sc.range(10,100).map(x=>{val z=x%2;(x+2,if(z+3>0) x*3 else x+4)}).collect()";
         String test2 = "sc.range(10,100)\n" +
+                ".map(i=>i+3)\n" +
+                ".collect()\n";
+        String test3 = "sc.range(10,100)\n" +
                 "  .map(i=>{val j=i%3;(i, if(j==0)i*10 else i*2)})\n" +
                 "  .map(r=>r._1+r._2)\n" +
                 "  .collect()\n";
-
+        System.out.println("***output for test1***");
         run(test1);
+        System.out.println("***output for test2***");
         run(test2);
+        System.out.println("***output for test3***");
+        run(test3);
     }
 }
